770b88da383176c92717410120029137a2912e75
Alexandre Abreu <contatoalexandre95@gmail.com>
2023-01-15 20:57:13 -0300

    Firtst commit building a class ContaCorrente

---
+++ bank/ContaCorrente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    public class ContaCorrente
+    {
+        public int numero_conta;
+        public string conta;
+        public string titutlar;
+        public double saldo;
+
+    }
+}
